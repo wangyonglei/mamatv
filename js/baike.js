65b2b3fc09e6bfbd2099a3213c8d5b6a70c308e1
wangyonglei <549749526@qq.com>
2016-12-24 17:38:20 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/baike.js
+++ js/baike.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	var el_nav = $('.videolist_nav .swiper-slide');
+	var el_nav = $('.baike_nav .swiper-slide');
 
 
-	var swiper1 = new Swiper('.videolist_nav', {
+	var swiper1 = new Swiper('.baike_nav', {
 		pagination: '.swiper-pagination',
 		slidesPerView: 'auto',
 		paginationClickable: true,
 		spaceBetween: 0,
 	});
-	var swiper2 = new Swiper('.videolist_content', {
+	var swiper2 = new Swiper('.baike_content', {
 		pagination: '.swiper-pagination',
 		slidesPerView: 'auto',
 		paginationClickable: true,
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		      }
 		      var nav_W = el_nav.eq(content_index).width();
 
-		      $('.videolist_nav').find('p').stop(true).animate({
+		      $('.baike_nav').find('p').stop(true).animate({
 		      			'width':nav_W,
 		      			'left':lengthz
 		      		});
@@ -39,13 +39,13 @@ $(document).ready(function() {
 
 // 默认的导航条状态
 	var morenW = el_nav.eq(0).width();
-	$('.videolist_nav').find('p').css({
+	$('.baike_nav').find('p').css({
 		'width':morenW+'px',
 		'left': '20px'
 	});
 
 // 导航与内容上下关联
-	// var el_content = $('.videolist_content .swiper-slide');
+	// var el_content = $('.baike_content .swiper-slide');
 	el_nav.click(function() {
 		var nav_index = el_nav.index($(this));
 		el_nav.eq(nav_index).addClass("nav_act").siblings().removeClass('nav_act');
@@ -60,11 +60,23 @@ $(document).ready(function() {
 			lengths = lengths + length;
 			
 		}
-		$('.videolist_nav').find('p').stop(true).animate({
+		$('.baike_nav').find('p').stop(true).animate({
 					'width':nav_width,
 					'left':lengths
 				});
 		
 
 	});	
+
+
+
+
+	// 切换标题
+	var flg = 0; //0为展开 1为闭合
+	$('.baike_title').click(function(){
+		
+		$(this).parent().toggleClass('addli')
+		$(this).find('.j').toggleClass('addj')
+		
+	})
 });
